5ead8f52ceee70c55a996e55b978ca305ebfb73b
bashu <basil.shubin@gmail.com>
2016-04-20 14:34:33 +0600

    more ``area`` charts

--- example/demos/area/views.py
+++ example/demos/area/views.py
@@ -177,3 +177,109 @@ class Area100PercentStacked(TemplateView):
         return context
 
 area100PercentStacked = Area100PercentStacked.as_view()
+
+
+class AreaStacked(Area100PercentStacked):
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Area100PercentStacked, self).get_context_data(*args, **kwargs)
+
+        chart = amSerialChart(
+            name='chart',
+            marginTop=10,
+            dataProvider=self.chartData,
+            categoryField="year",
+            pathToImages="%samcharts2/amcharts/images/" % settings.STATIC_URL,
+        )
+
+        chart.zoomOutButton = {
+            'backgroundColor': "#000000",
+            'backgroundAlpha': 0.15,
+        }
+
+        # AXES
+        # Category
+        chart.categoryAxis.gridAlpha = 0.07
+        chart.categoryAxis.axisColor = "#DADADA"
+        chart.categoryAxis.startOnAxis = True
+
+        # Value
+        valueAxis = amValueAxis(
+            title="Traffic incidents",
+            stackType="regular",  # this line makes the chart "stacked"
+            gridAlpha=0.07,
+        )
+        chart.addValueAxis(valueAxis)
+
+        # GUIDES are vertical (can also be horizontal) lines (or areas) marking some event.
+        # first guide
+        guide1 = amGuide(
+            category="2001",
+            lineColor="#CC0000",
+            lineAlpha=1,
+            dashLength=2,
+            inside=True,
+            labelRotation=90,
+            label="fines for speeding increased",
+        )
+        chart.categoryAxis.addGuide(guide1);
+
+        # second guide
+        guide2 = amGuide(
+            category="2007",
+            lineColor="#CC0000",
+            lineAlpha=1,
+            dashLength=2,
+            inside=True,
+            labelRotation=90,
+            label="motorcycle maintenance fee introduced",
+        )
+        chart.categoryAxis.addGuide(guide2);
+
+        # GRAPHS
+        # first graph
+        graph = amGraph(
+            type="line",
+            title="Cars",
+            valueField="cars",
+            balloonText="[[value]] ([[percents]]%)",
+            lineAlpha=1,
+            fillAlphas=0.6,  # setting fillAlphas to > 0 value makes it area graph
+            hidden=True,
+        )
+        chart.addGraph(graph)
+
+        # second graph
+        graph = amGraph(
+            type="line",
+            title="Motorcycles",
+            valueField="motorcycles",
+            balloonText="[[value]] ([[percents]]%)",
+            lineAlpha=1,
+            fillAlphas=0.6,
+        )
+        chart.addGraph(graph)
+
+        # third graph
+        graph = amGraph(
+            type="line",
+            title="Bicycles",
+            valueField="bicycles",
+            balloonText="[[value]] ([[percents]]%)",
+            lineAlpha=1,
+            fillAlphas=0.6,
+        )
+        chart.addGraph(graph)
+
+        # LEGEND
+        legend = amLegend(position="top")
+        chart.addLegend(legend)
+
+        # CURSOR
+        chartCursor = amChartCursor(zoomable=False, cursorAlpha=0)
+        chart.addChartCursor(chartCursor)
+
+        context['chart'] = chart
+        return context
+
+areaStacked = AreaStacked.as_view()
--- example/demos/urls.py
+++ example/demos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
 if 'demos.area' in settings.INSTALLED_APPS:
     urlpatterns = patterns('demos.area.views',
         url(r'100-stacked-area-chart/$', 'area100PercentStacked'),
+        url(r'stacked-area-chart/$', 'areaStacked'),
     )
