ecd0612205430ceed936ae49d553107b58525903
bashu <basil.shubin@gmail.com>
2016-04-21 11:07:27 +0600

    fixed broken urls

--- example/demos/urls.py
+++ example/demos/urls.py
@@ -6,17 +6,17 @@ urlpatterns = patterns('',
 
 if 'demos.area' in settings.INSTALLED_APPS:
     urlpatterns += patterns('demos.area.views',
-        url(r'100-stacked-area-chart/$', 'area100PercentStacked'),
-        url(r'stacked-area-chart/$', 'areaStacked'),
-        url(r'area-chart-with-time-based-data/$', 'areaWithTimeBasedData'),
+        url(r'^100-stacked-area-chart/$', 'area100PercentStacked'),
+        url(r'^stacked-area-chart/$', 'areaStacked'),
+        url(r'^area-chart-with-time-based-data/$', 'areaWithTimeBasedData'),
     )
 
 if 'demos.bar' in settings.INSTALLED_APPS:
     urlpatterns += patterns('demos.bar.views',
-        url(r'3d-bar-chart/$', 'bar3D'),
-        url(r'bar-and-line-chart-mix/$', 'barAndLineMix'),
-        url(r'clustered-bar-chart/$', 'barClustered'),
-        url(r'floating-bar-chart/$', 'barFloating'),
-        url(r'stacked-bar-chart/$', 'barStacked'),
-        url(r'bar-chart-with-background-image/$', 'barWithBackgroundImage'),
+        url(r'^3d-bar-chart/$', 'bar3D'),
+        url(r'^bar-and-line-chart-mix/$', 'barAndLineMix'),
+        url(r'^clustered-bar-chart/$', 'barClustered'),
+        url(r'^floating-bar-chart/$', 'barFloating'),
+        url(r'^stacked-bar-chart/$', 'barStacked'),
+        url(r'^bar-chart-with-background-image/$', 'barWithBackgroundImage'),
     )
